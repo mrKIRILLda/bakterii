a1e67db035b1d000908fba9a5c2d89cd5c752962
mrKIRILLda <kirch1537@gmail.com>
2025-02-14 18:54:36 +1100

    добавление цикла

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 words = [
   "книга",
   "учитель",
@@ -98,4 +100,28 @@ words = [
   "программирование",
   "математика"
 ]
+
+points = {'user': 0, 'comp': 0}
+
+while True:
+    a = random.randint(10, 110)
+    if is_odd(a):
+        continue
+    b = random.randint(10, 120)
+    if is_odd(b):
+        continue
+    op = random.choice(("+", "-"))
+    if op == "+":
+        ans = a + b
+        user = input(f"{a} + {b} = ")
+    else:
+        ans = a - b
+        user = input(f"{a} - {b} = ")
+    if user == str(ans):
+        print("Верно!")
+        points["player"] += 1
+    else:
+        print("Ты ошибся. Попробуй ещё разочек.")
+        points["comp"] += 1
+    print(f'Счёт: {points["user"]}:{points["comp"]}')
 
